949b03e762c912484dd664646e89e683eff34b62
favstats <fabio.votta@gmail.com>
2023-02-13 14:32:13 +0100

    add map progress and document code

--- R/parse_interests.R
+++ R/parse_interests.R
@@ -18,10 +18,10 @@
 #' @export
 fix_json <- function(include) {
     raw <- include %>%
-        str_replace_all("''", ",") %>%
+        stringr::str_replace_all("''", ",") %>%
         paste0("[", ., "]") %>%
-        str_remove_all('\\\\"') %>%
-        str_remove_all('\\\\')
+        stringr::str_remove_all('\\\\"') %>%
+        stringr::str_remove_all('\\\\')
 
     # print(raw)
     # counttt <<- counttt + 1
@@ -29,7 +29,7 @@ fix_json <- function(include) {
     if(raw == "[[]]" | raw == "[{}]" | raw == "[]") raw <- NA
 
     end <- raw %>%
-        map(~{
+        purrr::map(~{
             if (!is.na(.x)) {
                 parsed_json <- jsonlite::fromJSON(.x)
                 unlisted_x <- unlist(parsed_json) %>% na.omit()
@@ -68,16 +68,16 @@ fix_json <- function(include) {
 unnest_and_fix_dups <- function(dat, the_list, new_name) {
 
     dat %>%
-        unnest_longer({{the_list}})  %>%
+        tidyr::unnest_longer({{the_list}})  %>%
         ### this part is necessary because sometimes there are duplicate targeting criteria
-        mutate(!!rlang::sym(new_name) := ifelse(
-            str_ends({{the_list}}, "[:digit:]"),
-            str_sub({{the_list}}, 1, nchar({{the_list}})-1),
+        dplyr::mutate(!!rlang::sym(new_name) := ifelse(
+            stringr::str_ends({{the_list}}, "[:digit:]"),
+            stringr::str_sub({{the_list}}, 1, nchar({{the_list}})-1),
             {{the_list}})) %>%
-        group_by(archive_id) %>%
-        distinct(!!rlang::sym(new_name), .keep_all = T) %>%
-        ungroup() %>%
-        select(-{{the_list}})
+        dplyr::group_by(archive_id) %>%
+        dplyr::distinct(!!rlang::sym(new_name), .keep_all = T) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-{{the_list}})
 }
 
 
