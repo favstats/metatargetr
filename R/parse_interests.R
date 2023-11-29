7954475f94cc60f3fe1c93e7232fe7a6d2d32423
favstats <fabio.votta@gmail.com>
2023-11-29 09:59:42 +0100

    add google spending function

--- R/parse_interests.R
+++ R/parse_interests.R
@@ -1,93 +1,93 @@
-#' #' Parse JSON-formatted strings into named character vectors
-#' #'
-#' #' @param include A character vector of JSON-formatted strings
-#' #'
-#' #' @return A list of named character vectors, where each vector represents a parsed JSON object
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #' # Parse an example character vector
-#' #' example_json <- c("{\"city\":\"Berlin\",\"zip_code\":\"12345\"}",
-#' #'                   "{\"city\":\"Munich\",\"zip_code\":\"67890\"}")
-#' #' parsed_json <- fix_json(example_json)
-#' #'
-#' #' # Check the resulting list of named character vectors
-#' #' parsed_json
-#' #' }
-#' #'
-#' #' @export
-#' fix_json <- function(include) {
-#'     raw <- include %>%
-#'         stringr::str_replace_all("''", ",") %>%
-#'         paste0("[", ., "]") %>%
-#'         stringr::str_remove_all('\\\\"') %>%
-#'         stringr::str_remove_all('\\\\')
+#' Parse JSON-formatted strings into named character vectors
 #'
-#'     # print(raw)
-#'     # counttt <<- counttt + 1
+#' @param include A character vector of JSON-formatted strings
 #'
-#'     if(raw == "[[]]" | raw == "[{}]" | raw == "[]") raw <- NA
+#' @return A list of named character vectors, where each vector represents a parsed JSON object
 #'
-#'     end <- raw %>%
-#'         purrr::map(~{
-#'             if (!is.na(.x)) {
-#'                 parsed_json <- jsonlite::fromJSON(.x)
-#'                 unlisted_x <- unlist(parsed_json) %>% na.omit()
+#' @examples
+#' \dontrun{
+#' # Parse an example character vector
+#' example_json <- c("{\"city\":\"Berlin\",\"zip_code\":\"12345\"}",
+#'                   "{\"city\":\"Munich\",\"zip_code\":\"67890\"}")
+#' parsed_json <- fix_json(example_json)
 #'
-#'                 return(paste0(as.character(unlisted_x), ": ", unlisted_x %>% names))
-#'             } else {
-#'                 return(NA)
-#'             }
-#'         })
-#'
-#'     return(end)
+#' # Check the resulting list of named character vectors
+#' parsed_json
 #' }
 #'
-#' #' @title Unnest interest targeting data
-#' #' @description unnest and fix duplicates in interest targeting data from Ad Targeting Dataset
-#' #'
-#' #' The function unnests "include" and "exclude" columns in the Ad Targeting Dataset, and removes duplicates.
-#' #'
-#' #' @param dat a data frame
-#' #' @param the_list the column name to unnest
-#' #' @param new_name the name of the new column after unnesting and fixing duplicates
-#' #' @return a modified data frame with unnested and deduplicated values
-#' #'
-#' #' @examples
-#' #'
-#' #'### example usage:
-#' #'## make sure you have the variable 'archive_id' in your data
-#' #' ad_targeting_data %>%
-#' #'     rowwise() %>%
-#' #'     mutate(include_list = fix_json(include)) %>%
-#' #'     ungroup() %>%
-#' #'     ## the_list: the parsed list of JSON, new_name: what the parsed column should be called
-#' #'    unnest_and_fix_dups(the_list = include_list, new_name = "parsed_include")
-#' #'
-#' #' @export
-#' unnest_and_fix_dups <- function(dat, the_list, new_name) {
+#' @export
+fix_json <- function(include) {
+    raw <- include %>%
+        stringr::str_replace_all("''", ",") %>%
+        paste0("[", ., "]") %>%
+        stringr::str_remove_all('\\\\"') %>%
+        stringr::str_remove_all('\\\\')
+
+    # print(raw)
+    # counttt <<- counttt + 1
+
+    if(raw == "[[]]" | raw == "[{}]" | raw == "[]") raw <- NA
+
+    end <- raw %>%
+        purrr::map(~{
+            if (!is.na(.x)) {
+                parsed_json <- jsonlite::fromJSON(.x)
+                unlisted_x <- unlist(parsed_json) %>% na.omit()
+
+                return(paste0(as.character(unlisted_x), ": ", unlisted_x %>% names))
+            } else {
+                return(NA)
+            }
+        })
+
+    return(end)
+}
+
+#' @title Unnest interest targeting data
+#' @description unnest and fix duplicates in interest targeting data from Ad Targeting Dataset
 #'
-#'     dat %>%
-#'         tidyr::unnest_longer({{the_list}})  %>%
-#'         ### this part is necessary because sometimes there are duplicate targeting criteria
-#'         dplyr::mutate(!!rlang::sym(new_name) := ifelse(
-#'             stringr::str_ends({{the_list}}, "[:digit:]"),
-#'             stringr::str_sub({{the_list}}, 1, nchar({{the_list}})-1),
-#'             {{the_list}})) %>%
-#'         dplyr::group_by(archive_id) %>%
-#'         dplyr::distinct(!!rlang::sym(new_name), .keep_all = T) %>%
-#'         dplyr::ungroup() %>%
-#'         dplyr::select(-{{the_list}})
-#' }
+#' The function unnests "include" and "exclude" columns in the Ad Targeting Dataset, and removes duplicates.
 #'
+#' @param dat a data frame
+#' @param the_list the column name to unnest
+#' @param new_name the name of the new column after unnesting and fixing duplicates
+#' @return a modified data frame with unnested and deduplicated values
 #'
+#' @examples
 #'
+#'### example usage:
+#'## make sure you have the variable 'archive_id' in your data
+#' ad_targeting_data %>%
+#'     rowwise() %>%
+#'     mutate(include_list = fix_json(include)) %>%
+#'     ungroup() %>%
+#'     ## the_list: the parsed list of JSON, new_name: what the parsed column should be called
+#'    unnest_and_fix_dups(the_list = include_list, new_name = "parsed_include")
 #'
-#' #### example usage:
-#' ### make sure you have the variable 'archive_id' in your data
-#' # your_data %>%
-#' #     rowwise() %>%
-#' #     mutate(include_list = fix_json(include)) %>%
-#' #     ungroup() %>%
-#' #     ## the_list: the parsed list of JSON, new_name: what the parsed column should be called
-#' #    unnest_and_fix_dups(the_list = include_list, new_name = "parsed_include")
+#' @export
+unnest_and_fix_dups <- function(dat, the_list, new_name) {
+
+    dat %>%
+        tidyr::unnest_longer({{the_list}})  %>%
+        ### this part is necessary because sometimes there are duplicate targeting criteria
+        dplyr::mutate(!!rlang::sym(new_name) := ifelse(
+            stringr::str_ends({{the_list}}, "[:digit:]"),
+            stringr::str_sub({{the_list}}, 1, nchar({{the_list}})-1),
+            {{the_list}})) %>%
+        dplyr::group_by(archive_id) %>%
+        dplyr::distinct(!!rlang::sym(new_name), .keep_all = T) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-{{the_list}})
+}
+
+
+
+
+#### example usage:
+### make sure you have the variable 'archive_id' in your data
+# your_data %>%
+#     rowwise() %>%
+#     mutate(include_list = fix_json(include)) %>%
+#     ungroup() %>%
+#     ## the_list: the parsed list of JSON, new_name: what the parsed column should be called
+#    unnest_and_fix_dups(the_list = include_list, new_name = "parsed_include")
--- R/parse_location.R
+++ R/parse_location.R
@@ -1,95 +1,95 @@
-#' #' @title Parse Location from Ad Targeting Dataset
-#' #' @description A function to parse the location strings in the Ad Targeting Dataset and split into separate columns for each level of detail.
-#' #' @param .x A data.frame containing the location string
-#' #' @param loc_var A character string specifying the name of the column in .x that contains the location string
-#' #' @param type A character string specifying the prefix to add to each column of split location details. Default is "include". Should be "include" or "exclude".
-#' #' @param verbose A logical flag specifying whether to display a progress bar during processing. Default is `TRUE`.
-#' #' @return A data.frame with columns for each level of detail in the location.
-#' #' @importFrom purrr map_dfr
-#' #' @examples
-#' #' \dontrun{
-#' #'   ### create a dataset with unique include_location values
-#' #'   distinct_data <- targeting_data %>%
-#' #'     distinct(include_location)
-#' #'
-#' #'   #### parse the location data and join in original dataset
-#' #'   distinct_data %>%
-#' #'     parse_location(include_location, type = "include") %>%
-#' #'     right_join(targeting_data)
-#' #'
-#' #'   ###----####
-#' #'
-#' #'   ### create a dataset with unique exclude_location values
-#' #'   distinct_data <- targeting_data %>%
-#' #'     distinct(exclude_location)
-#' #'
-#' #'   #### parse the location data and join in original dataset
-#' #'   distinct_data %>%
-#' #'     parse_location(exclude_location, type = "exclude") %>%
-#' #'     right_join(targeting_data)
-#' #'
-#' #' }
-#' #' @export
-#' parse_location <- function(.x, loc_var, type = "include", verbose = T) {
+#' @title Parse Location from Ad Targeting Dataset
+#' @description A function to parse the location strings in the Ad Targeting Dataset and split into separate columns for each level of detail.
+#' @param .x A data.frame containing the location string
+#' @param loc_var A character string specifying the name of the column in .x that contains the location string
+#' @param type A character string specifying the prefix to add to each column of split location details. Default is "include". Should be "include" or "exclude".
+#' @param verbose A logical flag specifying whether to display a progress bar during processing. Default is `TRUE`.
+#' @return A data.frame with columns for each level of detail in the location.
+#' @importFrom purrr map_dfr
+#' @examples
+#' \dontrun{
+#'   ### create a dataset with unique include_location values
+#'   distinct_data <- targeting_data %>%
+#'     distinct(include_location)
 #'
-#'     if(verbose){
-#'         mapper <- map_dfr_progress
-#'     } else {
-#'         mapper <- purrr::map_dfr
-#'     }
+#'   #### parse the location data and join in original dataset
+#'   distinct_data %>%
+#'     parse_location(include_location, type = "include") %>%
+#'     right_join(targeting_data)
 #'
-#'     .x %>%
-#'         dplyr::select({{loc_var}}) %>%
-#'         dplyr::group_by(group = row_number(), .add = T) %>%
-#'         dplyr::group_split() %>%
-#'         mapper(~{
+#'   ###----####
 #'
-#'             parse_location_int(.x, {{loc_var}}, type)
+#'   ### create a dataset with unique exclude_location values
+#'   distinct_data <- targeting_data %>%
+#'     distinct(exclude_location)
 #'
-#'         }) %>%
-#'         dplyr::select({{loc_var}}, dplyr::contains("lvl1"), dplyr::contains("lvl2"), dplyr::contains("lvl3"), dplyr::contains("lvl4"))
+#'   #### parse the location data and join in original dataset
+#'   distinct_data %>%
+#'     parse_location(exclude_location, type = "exclude") %>%
+#'     right_join(targeting_data)
 #'
 #' }
-#'
-#' parse_location_int <- function(.x, loc_var, type = "include") {
-#'
-#'     val <- .x %>% dplyr::pull({{loc_var}})
-#'     if(is.na(val)) return(NULL)
-#'     json_format <- val %>%
-#'         jsonlite::fromJSON()
-#'
-#'     the_depth <- vec_depth(json_format)
-#'
-#'     framed <- json_format %>%
-#'         tibble::enframe()
-#'
-#'
-#'     if(the_depth == 2){
-#'         fin <- framed %>%
-#'             dplyr::rename(lvl1 = name) %>%
-#'             dplyr::select(-value)
-#'     } else if(the_depth == 3){
-#'         fin <- framed %>%
-#'             tidyr::unnest_longer(value) %>%
-#'             dplyr::rename(lvl2 = value_id) %>%
-#'             dplyr::select(-value)%>%
-#'             dplyr::rename(lvl1 = name)
-#'     } else if(the_depth == 4){
-#'         fin <- framed %>%
-#'             tidyr::unnest_longer(value) %>%
-#'             dplyr::rename(lvl2 = value_id) %>%
-#'             tidyr::unnest_longer(value) %>%
-#'             dplyr::rename(lvl3 = value_id) %>%
-#'             tidyr::unnest_longer(value) %>%
-#'             dplyr::rename(lvl1 = name,
-#'                    lvl4 = value)
-#'     }
-#'
-#'
-#'     fin <-  fin %>%
-#'         dplyr::select(-dplyr::contains("value_id")) %>%
-#'         dplyr::rename_all(~paste0(type, "_location_", .x)) %>%
-#'         dplyr::mutate({{loc_var}} := val)
-#'
-#'     return(fin)
-#' }
+#' @export
+parse_location <- function(.x, loc_var, type = "include", verbose = T) {
+
+    if(verbose){
+        mapper <- map_dfr_progress
+    } else {
+        mapper <- purrr::map_dfr
+    }
+
+    .x %>%
+        dplyr::select({{loc_var}}) %>%
+        dplyr::group_by(group = row_number(), .add = T) %>%
+        dplyr::group_split() %>%
+        mapper(~{
+
+            parse_location_int(.x, {{loc_var}}, type)
+
+        }) %>%
+        dplyr::select({{loc_var}}, dplyr::contains("lvl1"), dplyr::contains("lvl2"), dplyr::contains("lvl3"), dplyr::contains("lvl4"))
+
+}
+
+parse_location_int <- function(.x, loc_var, type = "include") {
+
+    val <- .x %>% dplyr::pull({{loc_var}})
+    if(is.na(val)) return(NULL)
+    json_format <- val %>%
+        jsonlite::fromJSON()
+
+    the_depth <- vec_depth(json_format)
+
+    framed <- json_format %>%
+        tibble::enframe()
+
+
+    if(the_depth == 2){
+        fin <- framed %>%
+            dplyr::rename(lvl1 = name) %>%
+            dplyr::select(-value)
+    } else if(the_depth == 3){
+        fin <- framed %>%
+            tidyr::unnest_longer(value) %>%
+            dplyr::rename(lvl2 = value_id) %>%
+            dplyr::select(-value)%>%
+            dplyr::rename(lvl1 = name)
+    } else if(the_depth == 4){
+        fin <- framed %>%
+            tidyr::unnest_longer(value) %>%
+            dplyr::rename(lvl2 = value_id) %>%
+            tidyr::unnest_longer(value) %>%
+            dplyr::rename(lvl3 = value_id) %>%
+            tidyr::unnest_longer(value) %>%
+            dplyr::rename(lvl1 = name,
+                   lvl4 = value)
+    }
+
+
+    fin <-  fin %>%
+        dplyr::select(-dplyr::contains("value_id")) %>%
+        dplyr::rename_all(~paste0(type, "_location_", .x)) %>%
+        dplyr::mutate({{loc_var}} := val)
+
+    return(fin)
+}
