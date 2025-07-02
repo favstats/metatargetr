69fcaeeb359afcc756c636981a249914be66ec32
favstats <fabio.votta@gmail.com>
2025-07-02 18:30:16 +0200

    fixing some errors and warnings

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+utils::globalVariables(c(".", "archive_id", "value", "value_id", "tframe", "ds",
+                         "file_name", "filename", "desc", "tag", "total_spend",
+                         "perc_spend", "item_type", "event", "event_time",
+                         "snapshot", "data", "no_data", "release", "name",
+                         "hash", "thefiles", "unique_counter", "hash_table_rows"))
+
+
 walk_progress <- function(.x, .f, ...) {
     .f <- purrr::as_mapper(.f, ...)
     pb <- progress::progress_bar$new(
